f3a884f4f3e5bab4b40bef905fa06fca1878ea3e
xiaohuyi <xiaohuyi@antiy.cn>
2024-07-23 15:54:25 +0800

    ok

--- init.sh
+++ init.sh
@@ -1,7 +1,18 @@
 
 # !/usr/bin/bash
 
+echo "start make"
 
-new_image="$ALIYUN_REGISTRY/$ALIYUN_NAME_SPACE/sia_python:3.6.15"
-docker build . --file Dockerfile --tag $new_image
-docker push $new_image
+# new_image="$ALIYUN_REGISTRY/$ALIYUN_NAME_SPACE/sia_python:3.6.15"
+# docker build . --file Dockerfile --tag $new_image 
+# docker push $new_image
+
+# echo "开始清理磁盘空间"
+# echo "=============================================================================="
+# df -hT
+# echo "=============================================================================="
+# docker rmi $new_image
+# echo "磁盘空间清理完毕"
+# echo "=============================================================================="
+# df -hT
+# echo "=============================================================================="  
