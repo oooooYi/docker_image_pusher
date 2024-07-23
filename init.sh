a543f7fcd58028e69f1eccaa40573bdaf0ee6b36
xiaohuyi <xiaohuyi@antiy.cn>
2024-07-23 15:48:36 +0800

    test

--- init.sh
+++ init.sh
@@ -2,16 +2,6 @@
 # !/usr/bin/bash
 
 
-docker login -u $ALIYUN_REGISTRY_USER -p $ALIYUN_REGISTRY_PASSWORD $ALIYUN_REGISTRY
-new_image="$ALIYUN_REGISTRY/$ALIYUN_NAME_SPACE/python:test"
+new_image="$ALIYUN_REGISTRY/$ALIYUN_NAME_SPACE/sia_python:3.6.15"
 docker build . --file Dockerfile --tag $new_image
 docker push $new_image
-echo "开始清理磁盘空间"
-echo "=============================================================================="
-df -hT
-echo "=============================================================================="
-docker rmi $new_image
-echo "磁盘空间清理完毕"
-echo "=============================================================================="
-df -hT
-echo "=============================================================================="  
